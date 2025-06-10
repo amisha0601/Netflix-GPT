a8f11f705397743e7eb321441058af33c6d6074a
Amisha Singh <amishaaworks06@gmail.com>
2025-06-10 13:04:15 +0530

    set up Firebase Authentication for login page.

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional 
+const firebaseConfig = {
+  apiKey: "AIzaSyBp2BFFkaZOoZvfVHqJxNKd89HezUCyoXs",
+  authDomain: "netflixgpt-d0d95.firebaseapp.com",
+  projectId: "netflixgpt-d0d95",
+  storageBucket: "netflixgpt-d0d95.firebasestorage.app",
+  messagingSenderId: "349709234647",
+  appId: "1:349709234647:web:6d6a45606cb2a4601f9b92",
+  measurementId: "G-E6H65KKY46"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
