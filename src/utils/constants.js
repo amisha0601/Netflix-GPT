108cbcf969469d6e4c64e5b1686105070f985235
Amisha Singh <amishaaworks06@gmail.com>
2025-07-16 21:12:49 +0530

    Creating your own custom hook.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,14 @@
-export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const USER_AVATAR = "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+export const USER_AVATAR =
+  "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-export const API_OPTIONS =  {
-  method: 'GET',
+export const API_OPTIONS = {
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZWU4ZDQ1OGU0NjUzOWMzNTAzZWNjYTdiZjc1NDMyZiIsIm5iZiI6MTc0OTgxMDA1My42MjEsInN1YiI6IjY4NGJmYjg1ZjZlZDExNzg0MjM0M2NhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l_ln-udzugjvBPbulK26CLY-BHDecVotRh8wdIIOuLE'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZWU4ZDQ1OGU0NjUzOWMzNTAzZWNjYTdiZjc1NDMyZiIsIm5iZiI6MTc0OTgxMDA1My42MjEsInN1YiI6IjY4NGJmYjg1ZjZlZDExNzg0MjM0M2NhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l_ln-udzugjvBPbulK26CLY-BHDecVotRh8wdIIOuLE",
+  },
 };
