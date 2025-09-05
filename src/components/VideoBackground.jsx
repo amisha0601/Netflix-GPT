342b45ad2ade508313ff99204ad4b2455ce6f86d
Amisha Singh <amishaaworks06@gmail.com>
2025-09-05 15:54:58 +0530

    Creating custom hook for Trailer video.

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,29 +1,10 @@
-import React, { useEffect } from "react";
-import {API_OPTIONS} from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const trailerVideo = useSelector(store => store.movies?.trailerVideo);
-  const dispatch = useDispatch();
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
 
-
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/1087192/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    dispatch(addTrailerVideo (trailer))
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+  useMovieTrailer(movieId);
 
   return (
     <div className="w-screen aspect-video overflow-hidden">
