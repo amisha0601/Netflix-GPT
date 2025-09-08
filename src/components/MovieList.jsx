fe44bcfc1b79caf1e9155cff7ab33118e4e776fa
Amisha Singh <amishaaworks06@gmail.com>
2025-09-08 22:12:21 +0530

    Show Popular Movies and Top Rated Movies list on browse page.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 const MovieList = ({ title, movies = [] }) => {
   console.log(movies);
   return (
-    <div className="px-6 ">
+    <div className="pl-6 ">
       <h1 className="text-2xl pt-5 pb-2.5 text-white">{title}</h1>
       <div className="flex overflow-x-scroll no-scrollbar">
         <div className="flex">
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -6,21 +6,26 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
     <div className=" bg-black">
-    <div className="-mt-60 pl-6 relative z-20">
-           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies || []} />
-      <MovieList title={"Trending"} movies={movies.nowPlayingMovies || []} />
-      <MovieList title={"Popular"} movies={movies.nowPlayingMovies || []} />
-      <MovieList
-        title={"Upcoming Movies"}
-        movies={movies.nowPlayingMovies || []}
-      />
-      <MovieList
-        title={"Horror Movies"}
-        movies={movies.nowPlayingMovies || []}
-      />
-      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies || []} />
-    </div>
- 
+      <div className="-mt-60 pl-6 relative z-20">
+        <MovieList
+          title={"Now Playing"}
+          movies={movies.nowPlayingMovies || []}
+        />
+        <MovieList title={"Popular"} movies={movies.popularMovies || []} />
+        <MovieList title={"Top Rated"} movies={movies.topRatedMovies || []} />
+        <MovieList
+          title={"Upcoming Movies"}
+          movies={movies.nowPlayingMovies || []}
+        />
+        <MovieList
+          title={"Horror Movies"}
+          movies={movies.nowPlayingMovies || []}
+        />
+        <MovieList
+          title={"Now Playing"}
+          movies={movies.nowPlayingMovies || []}
+        />
+      </div>
     </div>
   );
 };
