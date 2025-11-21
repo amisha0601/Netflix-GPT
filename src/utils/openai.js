628efe48f885c239d5c566c004f21e9115e833ef
Amisha Singh <amishaaworks06@gmail.com>
2025-11-22 02:13:13 +0530

    fixed.

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { OPENAI_KEY } from './constants';
 
 const client = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY, 
+  apiKey: OPENAI_KEY, 
   dangerouslyAllowBrowser: true,
 }); 
 
